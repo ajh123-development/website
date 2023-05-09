ce28fb068f84dde58e9ea036b2de6b80daa575a3
ajh123 <ajh123@users.noreply.github.com>
2023-05-09 17:28:01 +0100

    Become a Cloudflare Page + better search UI.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,21 @@ import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://minersonline.tk`,
-	vite: {
-		resolve: {
-			preserveSymlinks: true
-		}
-	}
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react()],
+  site: `https://minersonline.tk`,
+  vite: {
+    resolve: {
+      preserveSymlinks: true
+    }
+  },
+  output: "server",
+  adapter: cloudflare()
 });
