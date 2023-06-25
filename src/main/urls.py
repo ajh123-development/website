d32b8c05312361aa4a64f7544f4eab0eded30017
ajh123 <ajh123@users.noreply.github.com>
2023-06-25 20:31:05 +0100

    pass reset

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='main_home'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
