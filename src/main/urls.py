d336cd105e466a2cf382065443768336189b9d7d
ajh123 <ajh123@users.noreply.github.com>
2023-06-26 19:30:46 +0100

    Profile page

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='main_home'),
+    path('accounts/profile/', views.profile, name='main_profile'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,12 @@
-from django.http import Http404
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from blog.models import Post
 
 
 def home(request):
 	posts = Post.objects.all()
 	return render(request, "index.html", {"post_list": posts})
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
