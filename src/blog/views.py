133f24fcf6f4e9a62c593e259d9614291f12f33e
ajh123 <ajh123@users.noreply.github.com>
2023-07-26 18:00:36 +0100

    error pages

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 import markdown as md
 from .models import Post, Category
 
 def post_detail(request, category = None, slug = None):
     if category is not None and slug is None:
-        categoryObj: Category = Category.objects.get(slug=category)
+        categoryObj: Category = get_object_or_404(Category, slug=category)
         posts = Post.objects.filter(category=categoryObj)
         return render(request, "blog/category_view.html", {
             "post_list": posts,
             "category": categoryObj
         })
     elif slug is not None:
-        categoryObj: Category = Category.objects.get(slug=category)
-        post: Post = Post.objects.get(slug=slug)
-        if post.category.all().contains(categoryObj):
+        categoryObj: Category = get_object_or_404(Category, slug=category)
+        post: Post = get_object_or_404(Post, slug=slug)
+        if post is not None and post.category.all().contains(categoryObj):
             mdc = md.Markdown(extensions=['markdown.extensions.fenced_code', 'toc'])
             content = mdc.convert(post.content)
             return render(request, "blog/post_view.html", {
@@ -22,3 +23,5 @@ def post_detail(request, category = None, slug = None):
                 "post": post,
                 "content": content
             })
+        raise Http404
+    raise Http404
