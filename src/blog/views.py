ed107b0a61f6d28f97fc34de64c419398b564048
ajh123 <ajh123@users.noreply.github.com>
2023-07-26 10:59:40 +0100

    Re add privacy policy support

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
 import markdown as md
 from .models import Post, Category
-
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/index.html'
 
 def post_detail(request, category = None, slug = None):
     if category is not None and slug is None:
@@ -20,9 +15,10 @@ def post_detail(request, category = None, slug = None):
         post: Post = Post.objects.get(slug=slug)
         if post.category.all().contains(categoryObj):
             mdc = md.Markdown(extensions=['markdown.extensions.fenced_code', 'toc'])
-            mdc.convert(post.content)
+            content = mdc.convert(post.content)
             return render(request, "blog/post_view.html", {
                 "toc": mdc.toc,
                 "category": category,
-                "post": post
+                "post": post,
+                "content": content
             })
